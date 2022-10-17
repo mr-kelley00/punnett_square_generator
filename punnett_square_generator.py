1e3172bf38a82a2b52093e8c147342d70d064b18
Ryan Kelley <kelleyr@duvalschools.org>
2022-10-17 11:46:07 -0400

    v0.1
    Declared string variables to store parent allele information.

--- punnett_square_generator.py
+++ punnett_square_generator.py
@@ -1,1 +1,9 @@
-# Punnett Square Generator, Ryan Kelley, 11:43AM, 10/17/22, v0.0 
+# Punnett Square Generator, Ryan Kelley, 11:45AM, 10/17/22, v0.1
+
+# String Variables to store parent allele's. 
+parent0_allele0 = ""
+parent0_allele1 = ""
+parent1_allele0 = ""
+parent1_allele1 = ""
+
+
