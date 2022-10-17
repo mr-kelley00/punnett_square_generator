c217aa3cba7bec2928e61e7c61e9788fefe3761d
Ryan Kelley <kelleyr@duvalschools.org>
2022-10-17 11:53:23 -0400

    v0.2
    Added offspring allele variables.
    
    Display Punnett Square on screen with updated allele information.

--- punnett_square_generator.py
+++ punnett_square_generator.py
@@ -1,9 +1,25 @@
-# Punnett Square Generator, Ryan Kelley, 11:45AM, 10/17/22, v0.1
+# Punnett Square Generator, Ryan Kelley, 11:52AM, 10/17/22, v0.2
 
-# String Variables to store parent allele's. 
-parent0_allele0 = ""
-parent0_allele1 = ""
-parent1_allele0 = ""
-parent1_allele1 = ""
+# String Variables to store parent alleles. 
+parent0_allele0 = "A"
+parent0_allele1 = "a"
+parent1_allele0 = "a"
+parent1_allele1 = "a"
 
+# String Variables to store the offspring alleles. 
 
+# Display the Punnett Square
+print(f"+=================+=================+")
+print(f"+                 |                 +")
+print(f"+                 |                 +")
+print(f"+       {offspring_allele0}{offspring_allele1}        |     {offspring_allele2}{offspring_allele3}                 +")
+print(f"+                 |                 +")
+print(f"+                 |                 +")
+print(f"+=================+=================+")
+print(f"+                 |                 +")
+print(f"+                 |                 +")
+print(f"+       {parent0_allele0}{parent0_allele1}        |                 +")
+print(f"+                 |                 +")
+print(f"+                 |                 +")
+
+print(f"+=================+=================+")
